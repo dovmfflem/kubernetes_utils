3a2960143fb0133f976672e822c05f768b1fef4b
dovmfflem <dovmfflem@naver.com>
2020-01-07 16:56:56 +0900

    setting:

---
+++ join.sh
@@ -0,0 +1,5 @@
+kubeadm join
+10.0.1.116:6443 --token
+ zub92x.0v0cr8a4zdhaa2
+--discovery-token-ca-cert-hash sha256:
+b2ee96782c9fce1eb33e491d0f4e03fdc682d8c1196c273ab602cd35f12150f8
--- token_hash_viewer.sh
+++ token_hash_viewer.sh
@@ -1,5 +1,13 @@
-echo "kubeadm join" > join.txt
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubeadm token list -o yaml | grep token: | tr ":" "=" | tr -d ' ' >> join.txt
 
-openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' >> join.txt
+echo "kubeadm join" > join.sh
+
+echo "10.0.1.116:6443 --token" >> join.sh 
+
+kubeadm token list -o yaml | grep token: | tr -d "token:" >> join.sh
+
+echo "--discovery-token-ca-cert-hash sha256:" >> join.sh
+
+openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' >> join.sh
